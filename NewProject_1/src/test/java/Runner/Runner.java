2fa4debc7961a5cefa5f87ff286895ae9f90446b
pravin shetty <pravin.sh9@gmail.com>
2019-07-01 14:10:14 +0530

    pom

---
+++ NewProject_1/src/test/java/Runner/Runner.java
@@ -0,0 +1,31 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+
+
+	
+	@RunWith(Cucumber.class)
+	@CucumberOptions(
+
+				//format = {"pretty", "json:target/"},
+			 monochrome = false, 
+			 
+			 //format = {"pretty", "json:target/cucumber.json"},
+			 plugin={"html:target/cucumber-html-report"},
+			 	//plugin = {"pretty", "html:target/cucumber-htmlreport","pretty:target/cucumber-report.json"},
+		        features = {"features"},
+				glue = {"stepDefination"}
+		    )
+	
+	
+	public class Runner{
+		
+		
+	}
+
+
